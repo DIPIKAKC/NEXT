8a7ba5cb1f05303b15beab6ac61de569fedc6467
Dipika K.C <dipikak0323@gmail.com>
2025-12-19 23:52:11 +0545

    next basics

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
+import { Posts } from "@/models/model";
+import axios from "axios";
 
-export default function PostsPage() {
-    const posts = [
-        { id: 1, title: 'hi' },
-        { id: 2, title: 'bye' }
-    ]
-    return (
-        <div>
-            {posts.map((post) => {
-                return <div key={post.id}>
-                    {post.title}
-                </div>
-            })}
+export default async function PostsPage() {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+
+  const posts = response.data;
+
+  return (
+    <div>
+
+
+      {posts.map((post: Posts) => {
+        return <div key={post.id}>
+          <h1>{post.title}</h1>
+          <p>{post.body}</p>
         </div>
-    )
+      })}
+
+
+
+
+    </div>
+  )
 }
