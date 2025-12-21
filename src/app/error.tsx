f065e973b7db1efb35dd763e0cb23a7a0df01943
Dipika K.C <dipikak0323@gmail.com>
2025-12-21 23:58:02 +0545

    next basics

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -3,6 +3,7 @@
 import { Button } from '@/components/ui/button';
 
 export default function Error({error, reset}:{error:Error, reset:()=>void}) {
+    console.log(error)
   return (
     <div>
       <h1 className='mb-3'>{error.message}</h1>
